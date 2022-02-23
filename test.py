afa05017f976ac34924c3cdd0a89a7b4c3516700
Low Tier God <domaic95@kingshigh.school.nz>
2022-02-24 10:58:49 +1300

    Exercise 1

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("Hello world")
+print(5^5)
+
+print("My name is Dominik Aicken")
+print("My teacher's name is Ms. White")
+print("The name of my school is King's High")
+print("My age is 15")
+print("I have 800+ hours in my favourite video game") #I have been playing this game since 2019
+print("In 25 years it will be 2047")
+print("In 100 years I will be 115 years old")
+print(658/7)
+print("The moon landing was 53 years ago")
+print("My favourite videogame is Dead By Daylight")
+print("My favourite classes are 11JAP and 11DTE")
