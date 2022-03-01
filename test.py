3c36147e1d7c8a1e2cbb2dfef026e75b1f9418c5
Low Tier God <domaic95@kingshigh.school.nz>
2022-03-01 15:00:03 +1300

    test

--- test.py
+++ test.py
@@ -1,42 +1,74 @@
+print("Hello world")
+print(5**2 )
 #Exercise 4.1
+"""
+#1-5
 
-#print("Hello world")
-#print(5**2 )
-#print("\n")
-#print("My name is Dominik Aicken")
-#print("My teacher's name is Ms. White")
-#print("The name of my school is King's High")
-#print("My age is 15")
-#print("I have 800+ hours in my favourite video game") #I have been playing this game since 2019
-#print("In 25 years it will be 2047")
-#print("In 100 years I will be 115 years old")
-#print(658/7)
-#print("The moon landing was 53 years ago")
-#print("My favourite videogame is Dead By Daylight")
-#print("My favourite classes are 11JAP and 11DTE")
-#print("\n")
-#print(5+10.0)
-#print(10-5.0)
-#print(5/2)
-#print(5//2)
-#print(5%2)
-#print("Hello"*5)
-#print("\n")
+print("\n")
+print("My name is Dominik Aicken")
+print("My teacher's name is Ms. White")
+print("The name of my school is King's High")
+print("My age is 15")
+print("I have 800+ hours in my favourite video game") #I have been playing this game since 2019
 
+#6-11
+
+print("In 25 years it will be 2047")
+print("In 100 years I will be 115 years old")
+print(658/7)
+print("The moon landing was 53 years ago")
+print("My favourite videogame is Dead By Daylight")
+print("My favourite classes are 11JAP and 11DTE")
+
+
+Tests
+print("\n")
+print(5+10.0)
+print(10-5.0)
+print(5/2)
+print(5//2)
+print(5%2)
+print("Hello"*5)
+print("\n")
+"""
 #Exercise 4.4
+
+#1-5
+
 print("Never gonna give you up, never gonna let you down, never gonna run around and desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you")
 print("May the Force \nbe with you.")
 print("A string is a printed word in python, a string can be a word, letter or sentence printed in a python statement.")
 print("An integer is a number printed out as a whole number in python.")
 print("A float is like an integer - but it shows you the decimal numbers as well as the whole number.")
-print(543.0/3.0)
+
+#6-10
+
 print(543/3)
-print(543/3/100)
-print(2.0+3.0)
-print(5+7)
-print(230.7-780)
-print(49/17)
-print(30*18.4)
-print(7*2)
-print("Dominik Aicken"*10)
-print("\nDominik Aicken"*10)
+print(543//3)
+print(543%3)
+print(1000%9)
+print(434//10)
+
+
+#11-15
+
+print(3%2)
+print(5%5)
+print(555/2)
+print(123/2)
+print(123//2)
+
+
+#16-20
+
+print(123%2)
+print(764%2)
+print(165//2)
+print(90 + 55)
+print(280 - 92.77)
+print(18.23 - 7)
+print(8 / 2)
+
+#24-29
+
+#I need to put these into the correct order, they are not ordered correctly yet but this will be fixed.
