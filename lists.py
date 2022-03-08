55e57ba867adb13d2a392f94c39f4e776dfd059d
Low Tier God <100237036+Low-Tier-God@users.noreply.github.com>
2022-03-08 20:10:38 +1300

    lists update

--- lists.py
+++ lists.py
@@ -1,10 +1,30 @@
 #Exercise 8.7
 
 #1-5
-
+"""
 sequence = ["0","1","1","2","3","5","8","13","21","34"]
 print(sequence)
 fruits = ["apples","grapes","bananas","plums","kiwi"]
 print(fruits)
 youtubers = ["PewDiePie","Jacksetpiceye","Markiplier","ishowspeed"]
 print(youtubers)
+songs = []
+songs.append("Mr. Blue Sky")
+songs.append("Hey Ya!")
+songs.append("Don't Cry")
+songs.append("T.N.T")
+songs.append("Uptown Funk")
+print(songs)
+"""
+books = []
+book1 = input("Please add your fifth favourite book.\n")
+books.append({book1})
+book2 = input("Please add your fourth favourite book.\n")
+books.append({book2})
+book3 = input("Please add your third favourite book.\n")
+books.append({book3})
+book4 = input("Please add your second favourite book.\n")
+books.append({book4})
+book5 = input("Please add your favourite book.\n")
+books.append({book5})
+print(books)
