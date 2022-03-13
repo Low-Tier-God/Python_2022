10eed99b992b25a2e1b3bdc15b64e24790581f39
Low Tier God <domaic95@kingshigh.school.nz>
2022-03-14 10:56:48 +1300

    icecream maker works yay

--- loops.py
+++ loops.py
@@ -1,27 +1,44 @@
+#Exercise 9.2
+"""
 print("Hi, welcome to Ice Cream Maker")
 order_complete = False
 toppings_list = []
 
 toppings_available = ["vanilla","strawberry","chocolate","sprinkles","nuts","raisins","chocolate sauce","flake","m&ms"]
-print("Hello, customer, here are available toppings for your icecream:\n")
+print("Hello, customer, here are available toppings for your icecream:\n") #Prints avaiailable toppings
 print(toppings_available)
-print("\n")
+print("\n") #This all prints the toppings available
 
 topping_count = 0
-	
 
 while order_complete == False:
 	topping = input("What topping? - push enter to finish")
-if topping == "": 
-	print("Order Done")
-	order_complete = True
-elif topping in toppings_list: 
-	print("You already have that topping")
-else:
-	print("Great, adding it to the list")
-	toppings_list.append(topping)
+	if topping.lower() == "": 
+		print("Order Done")
+		order_complete = True #Shows the order is complete
+	elif topping.lower() not in toppings_available:
+		print("That topping is not in the available toppings.") #Makes it so that any topping entered not in the list isn't added
+	elif topping.lower() in toppings_list:
+		print("You already have that topping")
+	else:
+		print("Great, adding it to the list")
+		toppings_list.append(topping.lower()) #Makes it so that capitals in the inputs don't change the value and
+		#are printed in the same case each time
+
+
+
+
 
 print("\n")
 print("Here are your toppings:\n")
-print(toppings_list.join(","))
+print(*toppings_list,sep = "\n")
+"""
+#Exercise 9.3
 
+countup = []
+start = 0
+
+while countup != 100:
+	start = start + 2
+	start.append(countup)
+	print(countup)
