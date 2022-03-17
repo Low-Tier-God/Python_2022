020ef4a1c14236e70c0a09ede6c15caa61c881bf
Low Tier God <domaic95@kingshigh.school.nz>
2022-03-17 14:58:32 +1300

    dfas

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 from adventurelib import *
-
+"""
 @when("brush teeth")
 @when("brush")
 @when("clean teeth")
@@ -14,6 +14,69 @@ def comb_hair():
 		the gold hairbrush that you have selected from the 
 		in the red basket
 		""")
+"""
+
+
+
+space = Room("""
+	You are drifting in space. It feels very cold.
+	A slate-blue airlock sits completely silently in front of you,
+	its airlock open and waiting
+	""")
+
+airlock = Room("""
+	The bridge of the airlock is shiny and white, with thousands 
+	of small, red, blinking lights, to your left is the hallway, and the quarters are forward
+	""")
+
+cargo = Room("""
+	You enter the cargo room. There are thousands of storage
+	boxes holding all kinds of resources, to the left is the docking room""")
+
+docking = Room("""
+	The docking room is completely empty, with a ton of tempting buttons to press
+	the cargo room is now to your right""")
+
+hallway = Room("""
+	Upon entering the hallway, it is brightened by hundreds of lights to illuminate your path,
+	your available options here is back to the spaceship, to the bridge or the mess hall""")
+
+bridge = Room("""
+	Despite there being no reason to enter the bridge compartment, you enter anyway
+	There's nothing except a locked bridge entry way, the escape pods are forward from your position""")
+
+quarters = Room("""
+	The quarters are filled with beds and drawers of your fellow crewmates,
+	the spaceship room is behind you and the mess hall sits to your left""")
+
+mess_hall = Room("""
+	At the table are your fellow crewmates, though they don't look interested in conversing,
+	the hallway is behind you and the quarters are to your right""")
+
+escape_pods = Room("""
+	Why are you here? Do you seriously plan on escaping? Turn back to the bridge""")
+
+#variables
+current_room = space
+spaceship.east = hallway
+spaceship.south = quarters
+hallway.east == bridge
+hallway.north = cargo
+print(current_room)
+
+@when("enter airlock")
+@when("enter spaceship")
+@when("enter ship")
+def enter_airlock():
+	global current_room
+	if current_room is not space:
+		say("There is no airlock here")
+		return
+	else:
+		current_room = airlock
+		say("""You heave yourself into the airlock and slam your hand on the button to close the door.""")
+		print(current_room)
+		
 
 def main():
 	start()
--- task2.py
+++ task2.py
@@ -2,14 +2,42 @@ from adventurelib import *
 
 space = Room("""
 	You are drifting in space. It feels very cold.
-	A slate-blue spaceship sits completely silently to your left,
+	A slate-blue airlock sits completely silently to your left,
 	its airlock open and waiting
 	""")
 
-spaceship = Room("""
-	The bridge of the spaceship is shiny and white, with thousands 
-	of small, red, blinking lights
+airlock = Room("""
+	The bridge of the airlock is shiny and white, with thousands 
+	of small, red, blinking lights, to your left is the hallway, and the quarters are forward
 	""")
+
+cargo = Room("""
+	You enter the cargo room. There are thousands of storage
+	boxes holding all kinds of resources, to the left is the docking room""")
+
+docking = Room("""
+	The docking room is completely empty, with a ton of tempting buttons to press
+	the cargo room is now to your right""")
+
+hallway = Room("""
+	Upon entering the hallway, it is brightened by hundreds of lights to illuminate your path,
+	your available options here is back to the spaceship, to the bridge or the mess hall""")
+
+bridge = Room("""
+	Despite there being no reason to enter the bridge compartment, you enter anyway
+	There's nothing except a locked bridge entry way, the escape pods are forward from your position""")
+
+quarters = Room("""
+	The quarters are filled with beds and drawers of your fellow crewmates,
+	the spaceship room is behind you and the mess hall sits to your left""")
+
+mess_hall = Room("""
+	At the table are your fellow crewmates, though they don't look interested in conversing,
+	the hallway is behind you and the quarters are to your right""")
+
+escape_pods = Room("""
+	Why are you here? Do you seriously plan on escaping? Turn back to the bridge""")
+
 
 current_room = space
 print(current_room)
@@ -17,18 +45,16 @@ print(current_room)
 @when("enter airlock")
 @when("enter spaceship")
 @when("enter ship")
-def enter_spaceship():
+def enter_airlock():
 	global current_room
 	if current_room is not space:
 		say("There is no airlock here")
 		return
 	else:
-		current_room = spaceship
-		say("""You heave yourself into the spaceship and slam your hand on the button to close the door.""")
+		current_room = airlock
+		say("""You heave yourself into the airlock and slam your hand on the button to close the door.""")
 		print(current_room)
 
-def main():
-	start()
 
 if __name__ == '__main__':
 	main()
